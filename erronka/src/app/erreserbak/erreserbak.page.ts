aa076ca7ca42a1607b38c27f2c7e33b02626b4d9
Alejandro <arivass23@fpsanturtzilh.eus>
2024-12-20 09:23:39 +0100

    Nabegazioa commit

--- erronka/src/app/erreserbak/erreserbak.page.ts
+++ erronka/src/app/erreserbak/erreserbak.page.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./erreserbak.page.scss'],
 })
 export class ErreserbakPage implements OnInit {
+alertButtons: any;
 
   constructor() { }
 
